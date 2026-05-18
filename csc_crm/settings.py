87494fb6f1e2d450313829b73f535278de0220b9
subashininagalingam <subanagalingam.dev@gmail.com>
2026-05-18 12:02:52 +0530

    Fix: Fixed static and cloudinary setup

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -134,10 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_URL = '/media/'
