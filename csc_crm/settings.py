d2062ba8f0e3795ed713cf3189a9b28261744885
subashininagalingam <subanagalingam.dev@gmail.com>
2026-05-18 15:16:00 +0530

    Fix: Smtp error

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -156,6 +156,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 
 EMAIL_USE_TLS = True
 
+EMAIL_USE_TLS = False
+
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
