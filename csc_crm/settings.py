371c97dff7d3cd4d7fa3cb3f0f28e2797e7ba333
subashininagalingam <subanagalingam.dev@gmail.com>
2026-05-18 17:44:16 +0530

    Fix: smtp ssl

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -152,7 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
 EMAIL_USE_TLS = True
 
@@ -163,6 +163,8 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 30
 
 cloudinary.config(
     cloud_name=os.environ.get('CLOUD_NAME'),
