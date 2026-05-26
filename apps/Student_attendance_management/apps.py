af573d3f8edc5d36fa62390d1ffece59c8111602
subashininagalingam <subanagalingam.dev@gmail.com>
2026-05-26 16:10:58 +0530

    Feat: Added a attendance app

---
+++ apps/Student_attendance_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudentAttendanceManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Student_attendance_management'
